00279560b491382218ebbc1ca0625ff55d28edd3
Christopher <Chris.Medina93@gmail.com>
2023-09-25 13:21:23 -0700

    lists posted

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.use(express.json());
+
 const homePage = path.join(__dirname, 'index.html');
 app.get('/', (req, res)=> res.sendFile(homePage));
 
@@ -16,12 +18,63 @@ app.get('/dist/main.js.map', (req, res)=> res.sendFile(reactSourceMap));
 const styleSheet = path.join(__dirname, 'styles.css');
 app.get('/styles.css', (req, res)=> res.sendFile(styleSheet));
 
+app.put('/api/pets/:id', async(req,res,next)=>{
+  try {
+    const SQL=`
+    UPDATE pets 
+    SET user_id = $1, name= $2
+    WHERE id = $3 RETURNING *
+    `;
+    const response = await client.query(SQL, [req.body.user_id, req.body.name, req.params.id]);
+    res.send(response.rows[0]);
+  } catch (error) {
+    next(error);
+  }
+})
+
+app.get('/api/owners', async(req,res,next)=>{
+  try {
+    const response = await client.query('SELECT * FROM owners');
+    res.send(response.rows);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get('/api/pets', async(req,res,next)=>{
+  try {
+    const response = await client.query('SELECT * FROM pets');
+    res.send(response.rows);
+  } catch (error) {
+    next(error);
+  }
+})
+
 const init = async()=> {
   await client.connect();
   console.log('connected to database');
   const SQL = `
-    SQL SETUP AND SEED
+    DROP TABLE IF EXISTS pets;
+    DROP TABLE IF EXISTS owners;
+    CREATE TABLE owners(
+       id SERIAL PRIMARY KEY,
+      name VARCHAR (100) UNIQUE
+    );
+    CREATE TABLE pets(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR (100) UNIQUE,
+      user_id INTEGER REFERENCES owners (id)
+    );
+    INSERT INTO owners (name) VALUES ('Joe');
+    INSERT INTO owners (name) VALUES ('Jim');
+    INSERT INTO owners (name) VALUES ('Mary');
+    INSERT INTO owners (name) VALUES ('Beth');
+    INSERT INTO pets (name, user_id) VALUES ('Spot', (SELECT id FROM owners WHERE name = 'joe') );
+    INSERT INTO pets (name) VALUES ('Scruffy');
+    INSERT INTO pets (name) VALUES ('Rex');
+    INSERT INTO pets (name) VALUES ('Tiny');
   `;
+  await client.query(SQL);
   console.log('create your tables and seed data');
 
   const port = process.env.PORT || 3000;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,62 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from 'axios';
 
 const App = ()=> {
+  const [owners, setOwners] = useState([]);
+  const [pets, setPets] = useState([]);
+
+  useEffect(()=>{
+    const fetchOwners = async()=>{
+      const response = await axios.get('api/owners');
+      setOwners(response.data);
+    } 
+    fetchOwners();
+  },[]);
+
+
+  useEffect(()=>{
+    const fetchPets = async()=>{
+      const response = await axios.get('api/pets');
+      setPets(response.data);
+    } 
+    fetchPets();
+  },[]);
+
   return (
-    <h1>FullStack Template</h1>
+    <>
+    <h1>Pets & Owners</h1>
+    <main>
+      <div>
+        <h3>Owners ({owners.length})</h3>
+        <ul>
+          {
+            owners.map( owner =>{
+              return(
+                <li key = {owner.id}>
+                  {owner.name}
+                </li>
+              )
+            })
+          }
+        </ul>
+      </div>
+      <div>
+        <h3>Pets ({pets.length})</h3>
+        <ul>
+          {
+            pets.map( pet =>{
+              return(
+                <li key ={pet.id}>
+                  {pet.name}
+                </li>
+              )
+            })
+          }
+        </ul>
+      </div>
+    </main>
+    </>
   );
 };
 
